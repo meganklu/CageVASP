1cb59dd30b741a7e34aa153ea3bd5959765ad9d3
meganklu <mklu847@gmail.com>
2026-01-06 20:52:01 -0500

    changed formatting (spacing and number of decimals). added terminal usage + instructions

--- README.md
+++ README.md
@@ -1,21 +1,19 @@
 # XYZ to POSCAR Instructions
-## Make Cubic Unit Cell Input XYZ File
-1. Open metal–organic cage (MOC) as a CIF file in Mercury
-2. Pack the unit cell
-   - In the top-level menu, **Calculate > Packing/Slicing...**
-   - Under **Packing**, tick the **Pack** box on
-3. Save the structure as an XYZ file
-   - **File > Save As...**
-   - Select **XMol files (*.xyz)** as the file type
-4. Access the unit cell information
-   - In the top-level menu, **Display > More Information > Structure Information**
-   - Note the **Cell Lengths**, **Cell Angles**, **Cell Volume**
-## Convert XYZ to POSCAR File
-1. Add the XYZ file to the **in** directory
-2. In the **XYZToPOSCAR.java** class, call the `convert()` method in the `main()` method
-   - Use the file path to the **in** directory for the first parameter and enter a name for the output file as the second parameter (file path to the **out** directory)
-   - Input the **a**, **b**, and **c** values from the **Cell Lengths** as the a, b, and c parameters
-   - Input the a, b, and c values from the **Cell Angles** as the alpha, beta, and gamma parameters
-   - Input the **Cell Volume** value as the volume parameter
-   - Example line: `XYZToPOSCAR.convert("in/dimethyl_cage_unit_cell_xyz.txt", "out/dimethyl_cage_unit_cell_poscar.txt", 35.291, 17.2056, 35.416, 90, 117.236, 90, 19120.4);`
-3. Run the `XYZTOPOSCAR` to generate the POSCAR file
+## Mac Instructions
+1. Download the **xyz_to_poscar.py** file.
+    - Recommendation: Create a folder to store this file, the original xyz files, and the converted POSCAR files.
+2. Ensure that Python is on your computer.
+   - Open the **Terminal** app.
+   - Enter the command: `python --version`.
+   - If Python is installed, the output will be the version number (e.g., `Python 3.13.9`).
+   - If Python is not installed, the output will be `zsh: command not found: python`. Download the installer for your operating system on the [Python website](https://www.python.org/).
+3. In the **Terminal** app, access the folder containing the **xyz_to_poscar.py**.
+   - Type `cd <directory>` and press enter.
+   - To access the folder relative to your Home folder, write `cd ~/<filepath>` (e.g., `cd ~/Desktop/folder` for a folder in Desktop).
+   - If the directory change is successful, you will see the name of the folder before `%` and your cursor.
+4. Run the script.
+    - Enter the command: `python xyz_to_poscar.py <xyz file path> <POSCAR file path>`.
+    - The file paths is relative to the current directory. Use `../` to access the parent directory if the script and input file are in separate locations.
+    - Example: `python xyz_to_poscar.py "../in/zd001_Picture_1.xyz" "../out/zd001_Picture_1.poscar"`
+    - `File converted` indicates that the program is complete.
+   
--- xyztoposcar/xyz_to_poscar.py
+++ xyztoposcar/xyz_to_poscar.py
@@ -1,71 +1,77 @@
 import sys
 def convert(xyz, poscar):
-    with open(xyz, "r") as reader, open(poscar, "w") as writer:
-        # Comment in second line of XYZ, first line of POSCAR
-        count = int(reader.readline());
-        writer.write(reader.readline());
+    try:
+        with open(xyz, "r") as reader, open(poscar, "w") as writer:
+            # Comment in second line of XYZ, first line of POSCAR
+            count = int(reader.readline());
+            writer.write(reader.readline());
 
-        # Write scaling factor (universal scaling factor to 1 to enter the lattice vectors directly and avoid any additional scaling)
-        writer.write("1.0");
+            # Write scaling factor (universal scaling factor to 1 to enter the lattice vectors directly and avoid any additional scaling)
+            writer.write("1.0");
 
-        # Write lattice vectors (unit cell parameters)
-        a = 40;
-        a_formatted = f"{a:.10f}";
-        writer.write("\n\t" + a_formatted + " 0.0000000000 0.0000000000\n\t0.0000000000 " + a_formatted + " 0.0000000000\n\t0.0000000000 0.0000000000 " + a_formatted);
+            # Write lattice vectors (unit cell parameters)
+            a = 40;
+            writer.write(f"\n{a:>20.10f}{0:>21.10f}{0:>21.10f}\n{0:>20.10f}{a:>21.10f}{0:>21.10f}\n{0:>20.10f}{0:>21.10f}{a:>21.10f}");
 
-        # Generate dictionary of element to coordinate(s) (Key: element, Value: list of coordinates)
-        coords = {};
-        xsum = 0;
-        ysum = 0;
-        zsum = 0;
-        while line := reader.readline():
-            tokens = line.split();
-            element = tokens[0];
-            x = float(tokens[1]);
-            xsum += x;
-            y = float(tokens[2]);
-            ysum += y;
-            z = float(tokens[3]);
-            zsum += z;
-            coord = [x,y,z];
+            # Generate dictionary of element to coordinate(s) (Key: element, Value: list of coordinates)
+            coords = {};
+            xsum = 0;
+            ysum = 0;
+            zsum = 0;
+            while line := reader.readline():
+                tokens = line.split();
+                element = tokens[0];
+                x = float(tokens[1]);
+                xsum += x;
+                y = float(tokens[2]);
+                ysum += y;
+                z = float(tokens[3]);
+                zsum += z;
+                coord = [x,y,z];
 
-            if element in coords:
-                coords[element].append(coord);
-            else:
-                coords[element] = [coord];
+                if element in coords:
+                    coords[element].append(coord);
+                else:
+                    coords[element] = [coord];
 
-        # Write species names
-        elements = coords.keys();
-        writer.write("\n");
-        for element in elements:
-            writer.write("\t" + element);
+            # Write species names
+            elements = coords.keys();
+            writer.write("\n");
+            for element in elements:
+                writer.write(f"{element:>5}");
 
-        # Write ions per species
-        writer.write("\n");
-        for element in elements:
-            writer.write("\t" + str(len(coords[element])));
+            # Write ions per species
+            writer.write("\n");
+            for element in elements:
+                writer.write(f"{(len(coords[element])):>5}");
 
-        # Calculate coordinate shift
-        xavg = xsum / count;
-        xshift = a/2 - xavg;
-        yavg = ysum / count;
-        yshift = a/2 - yavg;
-        zavg = zsum / count;
-        zshift = a/2 - zavg;
+            # Calculate coordinate shift
+            xavg = xsum / count;
+            xshift = a/2 - xavg;
+            yavg = ysum / count;
+            yshift = a/2 - yavg;
+            zavg = zsum / count;
+            zshift = a/2 - zavg;
 
-        # Shift and write ion positions
-        writer.write("\nCartesian");
-        for element in elements:
-            for coord in coords[element]:
-                shifted_coord = str(coord[0] + xshift) + "\t" + str(coord[1] + yshift) + "\t"  + str(coord[2] + zshift);
-                writer.write("\n\t" + shifted_coord + " " + element);
+            # Shift and write ion positions
+            writer.write("\nCartesian");
+            for element in elements:
+                for coord in coords[element]:
+                    x = coord[0] + xshift;
+                    y = coord[1] + yshift;
+                    z = coord[2] + zshift;
+                    writer.write(f"\n{x:>16.9f}{y:>20.9f}{z:>20.9f}");
 
-        print("File converted")
+            print("File converted");
+    except FileNotFoundError:
+        print("Error: file not found");
+    except:
+        print("Error: check xyz file format");
 
-# if __name__ == "__main__":
-#     if len(sys.argv) == 4:
-#         convert(sys.argv[1], sys.argv[2], sys.argv[3]);
-#     else:
-#         print("Usage: python3 xyz_to_poscar.py <xyz> <poscar> <a>");
+if __name__ == "__main__":
+    if len(sys.argv) == 3:
+        convert(sys.argv[1], sys.argv[2]);
+    else:
+        print("Usage: python xyz_to_poscar.py <xyz> <poscar>");
 
-convert("../in/zd001_Picture_1.xyz", "../out/zd001_Picture_1.poscar");
+# convert("../in/zd001_Picture_1.xyz", "../out/zd001_Picture_1.poscar");
